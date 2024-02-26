4a25d9b646a642e44a8c6884f9b12320fd10f196
Pei Mu <ds1231h@gmail.com>
2024-02-26 21:35:31 +0000

    update skinner load tpc-h

--- tpch/skinner.load.sql
+++ tpch/skinner.load.sql
@@ -1,9 +1,9 @@
-load lineitem /Users/immanueltrummer/Data/tpch/tpc-h-sf-1/lineitem.csv | NULL;
-load nation /Users/immanueltrummer/Data/tpch/tpc-h-sf-1/nation.csv | NULL;
-load part /Users/immanueltrummer/Data/tpch/tpc-h-sf-1/part.csv | NULL;
-load partsupp /Users/immanueltrummer/Data/tpch/tpc-h-sf-1/partsupp.csv | NULL;
-load supplier /Users/immanueltrummer/Data/tpch/tpc-h-sf-1/supplier.csv | NULL;
-load region /Users/immanueltrummer/Data/tpch/tpc-h-sf-1/region.csv | NULL;
-load orders /Users/immanueltrummer/Data/tpch/tpc-h-sf-1/orders.csv | NULL;
-load customer /Users/immanueltrummer/Data/tpch/tpc-h-sf-1/customer.csv | NULL;
+load lineitem /home/pei/benchmarks/tpch-postgre/dbgen/out/lineitem.tbl | NULL;
+load nation /home/pei/benchmarks/tpch-postgre/dbgen/out/nation.tbl | NULL;
+load part /home/pei/benchmarks/tpch-postgre/dbgen/out/part.tbl | NULL;
+load partsupp /home/pei/benchmarks/tpch-postgre/dbgen/out/partsupp.tbl | NULL;
+load supplier /home/pei/benchmarks/tpch-postgre/dbgen/out/supplier.tbl | NULL;
+load region /home/pei/benchmarks/tpch-postgre/dbgen/out/region.tbl | NULL;
+load orders /home/pei/benchmarks/tpch-postgre/dbgen/out/orders.tbl | NULL;
+load customer /home/pei/benchmarks/tpch-postgre/dbgen/out/customer.tbl | NULL;
 compress;
