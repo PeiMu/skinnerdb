eb2ba449b007e4dba964f20927691ce7a7ae7170
Pei Mu <ds1231h@gmail.com>
2024-02-27 12:29:11 +0000

    upload tpch-3 result

--- tpch/skinner.load.sql
+++ tpch/skinner.load.sql
@@ -1,9 +1,9 @@
-load lineitem /home/pei/benchmarks/tpch-postgre/dbgen/out/lineitem.tbl | NULL;
-load nation /home/pei/benchmarks/tpch-postgre/dbgen/out/nation.tbl | NULL;
-load part /home/pei/benchmarks/tpch-postgre/dbgen/out/part.tbl | NULL;
-load partsupp /home/pei/benchmarks/tpch-postgre/dbgen/out/partsupp.tbl | NULL;
-load supplier /home/pei/benchmarks/tpch-postgre/dbgen/out/supplier.tbl | NULL;
-load region /home/pei/benchmarks/tpch-postgre/dbgen/out/region.tbl | NULL;
-load orders /home/pei/benchmarks/tpch-postgre/dbgen/out/orders.tbl | NULL;
-load customer /home/pei/benchmarks/tpch-postgre/dbgen/out/customer.tbl | NULL;
+load lineitem /home/pei/benchmarks/tpch-postgre/dbgen/out/sf-3/lineitem.tbl | NULL;
+load nation /home/pei/benchmarks/tpch-postgre/dbgen/out/sf-3/nation.tbl | NULL;
+load part /home/pei/benchmarks/tpch-postgre/dbgen/out/sf-3/part.tbl | NULL;
+load partsupp /home/pei/benchmarks/tpch-postgre/dbgen/out/sf-3/partsupp.tbl | NULL;
+load supplier /home/pei/benchmarks/tpch-postgre/dbgen/out/sf-3/supplier.tbl | NULL;
+load region /home/pei/benchmarks/tpch-postgre/dbgen/out/sf-3/region.tbl | NULL;
+load orders /home/pei/benchmarks/tpch-postgre/dbgen/out/sf-3/orders.tbl | NULL;
+load customer /home/pei/benchmarks/tpch-postgre/dbgen/out/sf-3/customer.tbl | NULL;
 compress;
