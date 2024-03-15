9881e44b9568a2a574f35eac579cb711f1a86043
Pei Mo <pei@luna.inf.ed.ac.uk>
2024-03-15 12:22:09 +0000

    upload jcc-h scripts

---
+++ jcch/skinner.load.sql
@@ -0,0 +1,9 @@
+load lineitem /home/pei/Project/benchmarks/JCC-H/out/lineitem.tbl | NULL;
+load nation /home/pei/Project/benchmarks/JCC-H/out/nation.tbl | NULL;
+load part /home/pei/Project/benchmarks/JCC-H/out/part.tbl | NULL;
+load partsupp /home/pei/Project/benchmarks/JCC-H/out/partsupp.tbl | NULL;
+load supplier /home/pei/Project/benchmarks/JCC-H/out/supplier.tbl | NULL;
+load region /home/pei/Project/benchmarks/JCC-H/out/region.tbl | NULL;
+load orders /home/pei/Project/benchmarks/JCC-H/out/orders.tbl | NULL;
+load customer /home/pei/Project/benchmarks/JCC-H/out/customer.tbl | NULL;
+compress;
